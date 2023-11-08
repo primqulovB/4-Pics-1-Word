b6c40154ea803d4ca76f74ea5a6680854cacc7f0
bahrom <bahromprimqulov2003@gmail.com>
2023-11-08 07:18:27 +0500

    commit 2023.08.11
    43 files added

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "4 Pics 1 word"
+include(":app")
+ 
